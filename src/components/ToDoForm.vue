4e2081fa440175a58fb351d1212aa872dd48ecec
April Day <april.day@gmail.com>
2021-06-11 23:40:59 +0100

    second commit

---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,27 @@
+<template>
+    <!-- need to have .prevent because there is no serve to post a callback-->
+    <form @submit.prevent="onSubmit">
+        <label for="new-todo-input">
+            What needs to be done?
+        </label>
+        <input
+            type="text"
+            id="new-todo-input"
+            name="new-todo"
+            autocomplete="off"
+        />
+        <button type="submit">
+            Add
+        </button>
+    </form>
+</template>
+
+<script>
+  export default {
+      methods: {
+          onSubmit() {
+              console.log('form submitted')
+          }
+      }
+  };
+</script>
